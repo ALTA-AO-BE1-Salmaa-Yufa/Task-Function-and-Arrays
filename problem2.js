2dbab0f63156618657890852cae55eb0c8488146
Salmaayufaa <salmaayufa@gmail.com>
2023-07-16 19:07:10 +0700

    problem2 done

---
+++ problem2.js
@@ -0,0 +1,16 @@
+// Buatlah sebuah program menggabungkan 2 array input, dalam problem ini jika terdapat nama yang sama di dalam 2 array tersebut, maka kamu hanya boleh memasukan satu nilai saja.
+// Sample Test Cases
+// Input: ['apel', 'nanas', 'anggur'], ['apel', 'pisang']
+// Output: ['apel', 'nanas', 'anggur', 'pisang']
+
+// Input: ['apel', 'nanas'], ['anggur', 'apel']
+// Output: ['apel', 'nanas', 'anggur', 'apel']
+
+function joinArrayRemoveDuplicate(arrayA, arrayB) {
+    const gabung  = arrayA.concat(array2)
+    
+}
+
+const arrayA = ["apel", "nanas", "anggur"];
+const arrayB = ["apel", "pisang"];
+console.log(joinArrayRemoveDuplicate(arrayA, arrayB));
